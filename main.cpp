721d11a7ac4234e5db11b9bfcb331b297376e4f7
adjiroblox <mnameyou@gmail.com>
2023-05-11 23:03:22 +0800

    continue tomorrow

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
+void userInput()
+{
+    std::string input;
+
+    std::getline(std::cin >> std::ws, input);
+}
+
 int main()
 {
-    std::cout << "hello world" << "\n";
+    userInput();
 
     return 0;
 }
