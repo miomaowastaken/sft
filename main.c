ced1357b2c74033a175b88482f40905740e45965
adjiroblox <mnameyou@gmail.com>
2023-05-12 11:44:52 +0800

    gigantic mess

--- main.c
+++ main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+#include "conio.h"
+
+const char* input()
+{
+    char buffer[BUFSIZ];
+    char text[BUFSIZ];
+
+    for(int i = 0; i < sizeof(buffer); i++)
+    {
+        buffer[i] = getch();
+
+        if(buffer[i] == 10)
+        {
+            buffer[i] = 0;
+            break;
+        }
+    }
+
+    *text = *buffer;
+    return text;
+}
 
 int main()
 {
-    printf("hello world");
+    printf("name: ");
+    const char* name = input();
+    printf("%s\n", name);
 
     return 0;
 }
